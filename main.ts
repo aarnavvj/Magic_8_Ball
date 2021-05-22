81061da6f8f2ceea1a8caa1f017c5b212cbee1f3
aarnavvj <81868372+aarnavvj@users.noreply.github.com>
2021-05-22 19:22:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+let Number2 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    Number2 = randint(1, 3)
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
